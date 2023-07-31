b0fa997c04b40ad2d85f6a4f4f0cfe85009e9fa3
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2023-07-31 05:32:03 +0530

    frontend is ready!

---
+++ src/scenes/loginPage/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return <Box>
+    <Box width="100%" backgroundColor={theme.palette.background.alt} 
+    p="1rem 6rem"
+    textAlign="center"
+    >
+       <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          Tumbler
+        </Typography>
+        </Box>
+
+        <Box 
+        width={isNonMobileScreens ? "50%" : "93"} 
+        p="2rem"
+        m="2rem auto"
+        borderRadius={theme.palette.background.alt}
+        >
+          <Typography fontWeight="500" variant='h5' sx={{ mb: "1.5rem" }}>
+            Welcome to Tumbler, the Social Media for Sociopaths!
+          </Typography>
+          <Form />
+
+        </Box>
+    </Box>
+  
+}
+
+export default LoginPage;
